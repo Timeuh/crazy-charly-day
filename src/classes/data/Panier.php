246f3141f343d74b88621f7c10b57104290ce7ee
drakoghir <gdardenne0@gmail.com>
2023-02-16 14:05:24 +0100

    feat : ajout du panier

---
+++ src/classes/data/Panier.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace custumbox\classes\data;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Panier extends Model
+{
+    protected $table = 'panier';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function produit() : BelongsTo {
+        return $this->belongsTo('custumbox\classes\data\Produit', 'id_produit');
+    }
+}
