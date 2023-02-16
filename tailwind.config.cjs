af27bf1113542fcecddb264129e479c59aafc126
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-16 12:10:06 +0100

    feat: bouge l'index php pour remplacer le html, et met tailwind en surveillance de l'index

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.php",
+    "./src/**/*.{js,ts,jsx,tsx,php}",
   ],
   theme: {
     extend: {},
