4a943cfff3f5f840a6c40d0ba720a0f8c6de2bec
BlakaObs <brenann.joly2@etu.univ-lorraine.fr>
2023-02-16 15:43:37 +0100

    feat: modification du panier ProduitAffichage

--- src/classes/afficheur/ProduitAffichage.php
+++ src/classes/afficheur/ProduitAffichage.php
@@ -2,15 +2,18 @@
 
 namespace custumbox\classes\afficheur;
 
+use custumbox\classes\data\Panier;
 use custumbox\classes\data\Produit;
 
 class ProduitAffichage
 {
-    public function execute() : string {
+    public function execute(): string
+    {
         $res = "<div>";
         $produit = Produit::where([
             ['id', '=', $_GET['id']]
         ])->first();
+        $idProduit = $produit->id;
         $categorie = $produit->categorie()->first();
         $res .= <<< EOT
             <div>
@@ -20,13 +23,30 @@ class ProduitAffichage
                 <p>{$produit->description}</p>
                 <p>{$produit->detail}</p>
                 <p>{$produit->prix}€</p>
-                <form action="index.php?action=panier" method="post">
+                <form action="index.php?action=produit&id=$idProduit" method="post">
                     <p>Quantité : <input type="number" name="quantite" id="quantite" value="1" min="1" max="99"></p>
-                    <button class="buttonPanier"><a href="index.php">Ajouter au panier</a></button>
+                    <button class="buttonPanier" type="submit">Ajouter au panier</button>
                 </form>
             </div>
             EOT;
         $res .= "</div>";
+
+        if (isset($_POST['quantite'])) {
+            $verif = Panier::where([
+                ['idclient', '=', 1],
+                ['idproduit', '=', $_GET['id']]
+            ])->first();
+            if ($verif != null) {
+                $verif->quantite = $verif->quantite + $_POST['quantite'];
+                $verif->update();
+            } else {
+                $ajt = new Panier();
+                $ajt->idClient = 1;
+                $ajt->idProduit = $_GET['id'];
+                $ajt->quantite = $_POST['quantite'];
+                $ajt->save();
+            }
+        }
 
         return $res;
     }
