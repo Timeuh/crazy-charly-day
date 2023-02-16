c1812093e258eddf0d1b2dc9e11ac1593676006d
Nathan <nathan.melbeck@gmail.com>
2023-02-16 11:09:42 +0100

    style: nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <nav>
+      <nav className=" drop-shadow-2xl bg-black text-slate-400">
         <ul>
-          {/* Mettre un logo ici?*/}
-          <li>Accueil</li>
-          <li>page1</li>
-          <li>page2</li>
-          <li>page3</li>
-          <li>a propos de nous</li>
+          <div className="flex flex-row justify-between">
+            {/* Mettre un logo ici?*/}
+            <img className="w-36"  src="../www/documents/court-circuit-logo-rond-jaune-vert.png" alt="logo"  />
+            <div className="flex flex-row ">
+              <button className="indexNavBtn"> <li>Accueil</li> </button>
+              <button className="indexNavBtn"> <li>page1</li> </button>
+              <button className="indexNavBtn"> <li>page2</li> </button>
+              <button className="indexNavBtn"> <li>page3</li> </button>
+              <button className="indexNavBtn"> <li>a propos de nous</li> </button>
+            </div>
+          </div>
         </ul>
       </nav>
       <h1>Notre Site</h1>
