798a3e3a4ea83d450e7fa0aa2a187c914d7ef762
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-16 17:51:22 +0100

    style: ajout du style pour le produit

--- src/classes/afficheur/ProduitAffichage.php
+++ src/classes/afficheur/ProduitAffichage.php
@@ -10,27 +10,34 @@ class ProduitAffichage
 {
     public function execute(): string
     {
-        $res = "<div>";
         $produit = Produit::where([
             ['id', '=', $_GET['id']]
         ])->first();
         $idProduit = $produit->id;
         $categorie = $produit->categorie()->first();
-        $res .= <<< EOT
-            <div>
-                <h2>{$produit->nom}</h2>
-                <h3>{$categorie->nom}</h3>
-                <img src="BD_img/{$produit->id}.jpg" alt="{$produit->nom}">
-                <p>{$produit->description}</p>
-                <p>{$produit->detail}</p>
-                <p>{$produit->prix}€</p>
-                <form action="index.php?action=produit&id=$idProduit" method="post">
-                    <p>Quantité : <input type="number" name="quantite" id="quantite" value="1" min="1" max="99"></p>
-                    <button class="buttonPanier" type="submit">Ajouter au panier</button>
-                </form>
+        $res = <<< EOT
+            <div class="flex flex-row justify-center items-center h-full">
+                <div class="border-2 my-4 mx-8 rounded-md border-emerald-900 p-4">
+                    <img src="BD_img/{$produit->id}.jpg" alt="{$produit->nom}" class="pt-12">
+                </div>
+                <div class="w-2/5 border-2 border-emerald-900 rounded-md space-y-12 p-4 m-4">
+                    <div>
+                        <h2 class="text-4xl font-bold text-yellow-500">{$produit->nom}</h2>
+                        <h3 class="text-xl italic font-bold text-emerald-900">{$categorie->nom}</h3>
+                        <p class="text-2xl py-4 text-yellow-500">{$produit->prix}€</p>
+                    </div>
+                    <div class="space-y-2 italic">
+                        <p>{$produit->description}</p>
+                        <p>{$produit->detail}</p>
+                    </div>
+                    <form action="index.php?action=produit&id=$idProduit" method="post" class="flex justify-around items-center">
+                        <p class="font-bold">Quantité <input type="number" name="quantite" id="quantite" value="1" min="1" max="99" 
+                        class="border-2 rounded-md border-yellow-500 text-center"></p>
+                        <button class="formSubmit" type="submit">Ajouter au panier</button>
+                    </form>
+                </div>
             </div>
             EOT;
-        $res .= "</div>";
 
         if (isset($_POST['quantite']) && isset($_SESSION['user'])) {
             $pannier = $_SESSION['user']->produits();
