71d732b3194dc17889080624f9f794edfbbeed2c
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-16 20:57:15 +0100

    style: ajoute un style au formulaire de connexion

--- src/classes/afficheur/Connexion.php
+++ src/classes/afficheur/Connexion.php
@@ -16,16 +16,29 @@ class Connexion extends Action
 
         if ($this->http_method == 'GET') {
             $res = "
-        <form id='sign' method='post' action='?action=connexion'>
-                    <h1>Inscription</h1>
-                    <label><b>login*</b><input type='text' name='login' placeholder='login' required></label>
-                    <label><b>Mot de passe*</b> <input type='password' name='pass' placeholder='Mot de passe' required></label>
-                    <input type='submit' id='inscr' value='INSCRIPTION'>";
+       <div class='bg-teal-800 h-screen'>";
             //S'il y a des erreurs on ajoutera une ligne supplementaire selon la nature de l'erreur renvoye
             if (isset($_GET['error'])) {
-                        $res .= "<p style='color:red'>Login ou mot de passe faux</p><br>";
+                $res .= "<h1 class='error'>Login ou mot de passe faux</h1>";
             }
-            $res .= "</form>";
+
+            $res .= "
+           <form id='sign' method='post' action='?action=connexion'>
+                <h1 class='text-center font-bold text-4xl pt-4 text-amber-400'>Connexion</h1>
+                <div class='flex justify-center'>
+                    <div class='flex flex-col max-w-80 m-0 p-8 text-center w-1/4'>
+                        <span class='formSpan'>
+                            <input class='formInput' type='text' name='login' placeholder=' login' required><label class='formLabel'>Login* </label>
+                        </span>
+                        <span class='formSpan'>
+                            <input class='formInput' type='password' name='pass' placeholder='Mot de passe' required><label class='formLabel'>Login* </label>
+                        </span>
+                        <input class='userFormSubmit' type='submit' id='inscr' value='Connexion'>
+                    </div>
+                </div>
+           </form>
+       </div>
+       ";
         }else
             if ($this->http_method == 'POST') {
                 $login = $_POST['login'];
