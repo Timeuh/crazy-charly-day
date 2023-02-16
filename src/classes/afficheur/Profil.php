2780c422fc63b589cd85cfc0d6a9a74458ecca27
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-16 22:44:45 +0100

    style: ajoute le style pour la page de profil

--- src/classes/afficheur/Profil.php
+++ src/classes/afficheur/Profil.php
@@ -2,17 +2,16 @@
 
 namespace custumbox\classes\afficheur;
 
-use custumbox\classes\data\Produit;
-
 class Profil
 {
     public function execute() : string {
         $res = "";
         $user = unserialize($_SESSION['user']);
         if ($user==null){
-            $res = "vous devez etre connecter";
-        }else{
-         $res =  $user->nom." ".$user->prenom;
+            $res = "<h1 class='error'>vous devez être connecté</h1>";
+        }else {
+            $res = "<div class='flex flex-col h-screen text-4xl text-yellow-500 bg-emerald-700 text-center'>
+                <h1><span>Nom : </span>" . $user->nom . "</h1> <h1><span>Prenom : </span>" . $user->prenom . "</h1></div>";
         }
         return $res;
     }
