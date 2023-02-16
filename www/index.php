ec7fb63a3f2f246a720a51b31439ccbcc29336e8
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-16 12:39:57 +0100

    feat: réorganisation de l'index

--- www/index.php
+++ www/index.php
@@ -1,6 +1,7 @@
 <?php
 namespace custumbox;
 require_once "../vendor/autoload.php";
+
 use custumbox\classes\dispatcher\Dispatcher;
 use Illuminate\Database\Capsule\Manager as DB;
 
@@ -15,21 +16,31 @@ $dispatch = new Dispatcher($action);
 $dispatch->run();
 
 print <<<HTML
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Crazy Charly Day</title>
+    <link href="styles/output.css" rel="stylesheet">
+</head>
+<body>
 <div>
-      <nav className=" drop-shadow-2xl bg-black text-slate-400">
+      <nav class="drop-shadow-2xl bg-black text-slate-400">
         <ul>
-          <div className="flex flex-row justify-between">
-            <img className="w-36"  src="../www/documents/court-circuit-logo-rond-jaune-vert.png" alt="logo"  />
-            <div className="flex flex-row ">
-              <button className="indexNavBtn"> <li>Accueil</li> </button>
-              <button className="indexNavBtn"> <li>page1</li> </button>
-              <button className="indexNavBtn"> <li>page2</li> </button>
-              <button className="indexNavBtn"> <li>page3</li> </button>
-              <button className="indexNavBtn"> <li>a propos de nous</li> </button>
+          <div class="flex flex-row justify-between">
+            <img class="w-36"  src="../www/documents/court-circuit-logo-rond-jaune-vert.png" alt="logo"  />
+            <div class="flex flex-row ">
+              <button class="indexNavBtn"> <li>Accueil</li> </button>
+              <button class="indexNavBtn"> <li>page1</li> </button>
+              <button class="indexNavBtn"> <li>page2</li> </button>
+              <button class="indexNavBtn"> <li>page3</li> </button>
+              <button class="indexNavBtn"> <li>a propos de nous</li> </button>
             </div>
           </div>
         </ul>
       </nav>
       <h1>Notre Site</h1>
     </div>
+</body>
+</html>
 HTML;
